78f1bd81fe326a42321d56a90edef11a27a245d9
Julia Victoria Cabral de Sena Silva <julia.silva@compasso.com.br>
2022-12-29 17:37:56 -0300

    KOANS

--- Curso JavaScript/scripts.js
+++ Curso JavaScript/scripts.js
@@ -124,3 +124,119 @@
 //Operadores lógicos NOT ( ! )
 //let candidatoRecusado = !podeAplicar;
 //console.log(candidatoRecusado);
+
+//Mini projeto
+
+// let a = 'vermelho';
+// let b = 'azul';
+// let aux;
+
+// aux = a;
+// a = b;
+// b = aux;
+
+// console.log(a);
+// console.log(b);
+
+// If.. Else
+
+// Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+// Se estiver entre 12:00 até 18:00 : Boa tarde!
+// Caso contrario Boa noite!
+
+
+// let hora = 10;
+// if (hora > 6 && hora < 12){
+//     console.log("Bom dia");
+
+// }
+// else if (hora > 12 && hora < 18){
+//     console.log("Boa tarde");
+// }
+// else{
+//     console.log("Boa noite");
+// }
+
+////////////////////////////////////////
+
+// let permissao; // comum, gerente, diretor
+
+// switch (permissao) {
+//     case 'comum':
+//     console.log('usuário comum');
+//     break;
+
+//     case 'gerente':
+//     console.log('usuário gerente');
+//     break;
+
+//     case 'diretor':
+//     console.log('usuário diretor');
+//     break;
+
+//     default :
+//     console.log('usuário não reconhecido!');
+// }
+
+/////////////////////////////////
+
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+// console.log('Estou aprendendo!');
+
+// 1. Laço For
+
+// for(let i = 1; i <= 5; i++) {
+//     console.log('Estou aprendendo!',i);
+// }
+
+// for(let i = 5; i >= 1; i--) {
+//     if(i % 2 !== 0){
+//     console.log(i);
+//     }
+// }
+
+// // While loop
+
+// let i = 5
+
+// while(i >= 5){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//         }
+//         i--;
+// }
+
+// Do... While
+
+// let i = 0;
+// do{
+//     console.log('digitando',i);
+//     i++;
+// } while (i < 10)
+
+//for-in
+
+// const pessoa = {
+//     nome: 'Julia',
+//     idade: 27
+// };
+
+// for(let chave in pessoa) {
+//     console.log(chave,pessoa);
+// }
+
+// const cores = ['Vermelho','Azul','Verde'];
+
+// for(let indice in cores) {
+//     console.log(indice,cores[indice])
+
+// }
+
+//for of
+
+// for(let cor of cores){
+//     console.log(cor);
+// }
